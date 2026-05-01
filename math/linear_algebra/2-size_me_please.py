b2594eb79c3323d03e628f6a8f98189c4a20a42e
lalalalily <ranaarasulzade@gmail.com>
2026-05-01 20:59:03 +0000

    huseyn

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    a = 0
-    for i in matrix:
-        b = 0
-        for j in i:
-            b+= 1
-        a+= 1
-    if type(j) == list:
-        c = len(j)
-        return[a,b,c]
-    return[a,b]
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
