08ea582655173c518a4afa3d67a5fe6d02b47c22
lalalalily <ranaarasulzade@gmail.com>
2026-05-01 21:29:46 +0000

    final

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -3,6 +3,15 @@
 
 
 def add_arrays(arr1, arr2):
+    """
+    Add two arrays element-wise.
+    Args:
+        arr1: A list of ints/floats
+        arr2: A list of ints/floats
+    Returns:
+        A new list with elements added element-wise,
+        or None if arr1 and arr2 are not the same shape
+    """
     if len(arr1) != len(arr2):
         return None
     result = []
