25baeb23a0fa182203d2149b7f650d4bb7411bda
lalalalily <ranaarasulzade@gmail.com>
2026-05-01 21:56:03 +0000

    new

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that calculates the shape of a numpy ndarray"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy ndarray as a tuple of integers"""
+    return matrix.shape
