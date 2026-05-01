e28fc4f3d6536c5a578647b9723ec1969b84bfe3
lalalalily <ranaarasulzade@gmail.com>
2026-05-01 21:37:56 +0000

    file 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -7,9 +7,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Args:
         mat1: A 2D list of ints/floats
         mat2: A 2D list of ints/floats
-        axis: 0 to concatenate vertically (rows), 1 to concatenate horizontally (columns)
+        axis: 0 to concatenate vertically (rows)
     Returns:
-        A new concatenated matrix, or None if matrices cannot be concatenated
+        A new concatenated matrix, or None if matrices 
     """
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
