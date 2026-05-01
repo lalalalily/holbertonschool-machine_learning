6e5f03bc61a8ceacc0695403d84eddce19f1208d
lalalalily <ranaarasulzade@gmail.com>
2026-05-01 21:58:57 +0000

    final

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Function that calculates the shape of a numpy ndarray"""
-import numpy as np
+
 
 
 def np_shape(matrix):
---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for concatenating numpy arrays."""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Concatenate two numpy arrays along a specific axis.
+    Args:
+        mat1: A numpy.ndarray
+        mat2: A numpy.ndarray
+        axis: 0 to concatenate vertically (rows), 1 to concatenate horizontally (columns)
+    Returns:
+        A new concatenated numpy.ndarray
+    """
+    return np.concatenate((mat1, mat2), axis=axis)
