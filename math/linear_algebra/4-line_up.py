5e9efbde1e03d1c69406c842e7bb0762e37626e0
lalalalily <ranaarasulzade@gmail.com>
2026-05-01 21:26:50 +0000

    task4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for adding arrays element-wise."""
+
+
+def add_arrays(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return None
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] - arr2[i])
+    return result
