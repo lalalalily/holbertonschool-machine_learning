7b0c8fd8804909b876c618885f044d36bf30be95
lalalalily <ranaarasulzade@gmail.com>
2026-05-08 19:28:50 +0000

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""Module for summation of i squared"""
+
+
 def summation_i_squared(n):
-    sum = 0
-    for i in range(n):
-        print(i)
-        sum+= (i+1)*(i+1)
-    return sum
+    """Calculates the sum of i^2 from 1 to n using the direct formula"""
+    if not isinstance(n, (int, float)) or n < 0:
+        return None
+    if n == 0:
+        return 0
+    result = (n * (n + 1) * (2 * n + 1)) / 6
+    return int(result)
