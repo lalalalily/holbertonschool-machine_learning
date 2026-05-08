dcccb5f3b157e4d02a5d25a826cf4becfda9b384
lalalalily <ranaarasulzade@gmail.com>
2026-05-08 19:32:00 +0000

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,8 @@
 
 def summation_i_squared(n):
     """Calculates the sum of i^2 from 1 to n"""
-    if not isinstance(n, int) or n < 0:
+    if not isinstance(n, int) or n < 1:
+        if n == 0:
+            return 0
         return None
     return (n * (n + 1) * (2 * n + 1)) // 6
