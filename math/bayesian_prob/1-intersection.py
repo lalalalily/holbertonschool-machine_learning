ebef5430137b808ad59cc1a31c92ad686efa2e25
lalalalily <ranaarasulzade@gmail.com>
2026-05-11 20:50:36 +0000

    intersection

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -4,17 +4,17 @@ import numpy as np
 
 
 def intersection(x, n, P, Pr):
-    """Calculates intersection of obtaining data with hypothetical probabilities"""
+    """Calculates intersection """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("x must be an ")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+        raise TypeError("Pr must be a numpy.ndarray")
     if np.any((P < 0) | (P > 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
     if np.any((Pr < 0) | (Pr > 1)):
