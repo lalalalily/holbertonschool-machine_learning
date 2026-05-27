9d4965e3f8c7cb6824e6aba7f482f1f318089e60
lalalalily <ranaarasulzade@gmail.com>
2026-05-27 22:44:10 +0000

    task

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -16,7 +16,7 @@ def bars():
     bottoms = np.zeros(3)
     # Plot each row (fruit) stacked on top of the previous ones
     for i in range(len(fruit)):
-        plt.bar(people, fruit[i], width=0.5, bottom=bottoms, 
+        plt.bar(people, fruit[i], width=0.5, bottom=bottoms,
                 color=colors[i], label=fruit_types[i])
         bottoms += fruit[i]   # Update the bottom position
     # Set the title and labels
