6dde42ec9ee559411ba78f7ae4a0653712029148
lalalalily <ranaarasulzade@gmail.com>
2026-05-27 22:08:12 +0000

    task

---
+++ math/plotting/0-line.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """
+    Plots a line graph with a solid red line and x-axis ranging from 0 to 10
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Generate x values corresponding to y indices (0 through 10)
+    x = np.arange(0, 11)
+
+    # Plot y as a solid red line ('r-')
+    plt.plot(x, y, 'r-')
+
+    # Force the x-axis limits to range exactly from 0 to 10
+    plt.xlim(0, 10)
+
+    # Display the final graph layout
+    plt.show()
+    
