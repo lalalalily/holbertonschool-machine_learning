3847b87d07c22ea3d6133293645ff4561bceee9b
lalalalily <ranaarasulzade@gmail.com>
2026-05-27 22:49:17 +0000

    task

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
+"""Module that contains a function to plot student grade frequency."""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def frequency():
 
+def frequency():
+    """Plots a histogram of student scores for a project."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    # your code here
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    # Define the bins every 10 units (from 0 to 100 based on typical grades)
+    # np.arange(0, 101, 10) creates: [0, 10, 20, ..., 100]
+    bins = np.arange(0, 101, 10)
+
+    # Plot the histogram with black outlines around the bars
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+
+    # Set X and Y axis labels
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    plt.title('Project A')
+
+    # Explicitly set the X-axis ticks to match the 10-unit bins
+    plt.xticks(bins)
+  
+    # Ensure the X-axis view limits tightly wrap around the bin range
     plt.xlim(0, 100)
-    plt.xticks(range(0, 101, 10))
-    plt.tight_layout()
-    plt.savefig('4-frequency.png')
+
+    # Set the title
+    plt.title('Project A')
+
+    plt.show()
