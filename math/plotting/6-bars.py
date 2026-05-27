ed39e81ee482d48b4ff9c225cffdd5bb6bfe67cd
lalalalily <ranaarasulzade@gmail.com>
2026-05-27 22:31:29 +0000

    task

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,39 @@
+6. Stacking Bars
+Complete the following source code to plot a stacked bar graph:
+
+fruit is a matrix representing the number of fruit various people possess
+The columns of fruit represent the number of fruit Farrah, Fred, and Felicia have, respectively
+The rows of fruit represent the number of apples, bananas, oranges, and peaches, respectively
+The bars should represent the number of fruit each person possesses:
+The bars should be grouped by person, i.e, the horizontal axis should have one labeled tick per person
+Each fruit should be represented by a specific color:
+apples = red
+bananas = yellow
+oranges = orange (#ff8000)
+peaches = peach (#ffe5b4)
+A legend should be used to indicate which fruit is represented by each color
+The bars should be stacked in the same order as the rows of fruit, from bottom to top
+The bars should have a width of 0.5
+The y-axis should be labeled Quantity of Fruit
+The y-axis should range from 0 to 80 with ticks every 10 units
+The title should be Number of Fruit per Person
+hbt-ml@Holberton-ML:~$ cat 6-bars.py
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bars():
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+Result
+
+hbt-ml@Holberton-ML:~$ cat 6-main.py
+#!/usr/bin/env python3
+
+bars = __import__('6-bars').bars
+
+bars()
+hbt-ml@Holberton-ML:~$ ./6-main.py
