3809b4c7007be1778a3126b3fbd3d7439dbb920d
lalalalily <ranaarasulzade@gmail.com>
2026-06-23 23:37:51 +0000

    final

--- supervised_learning/decision_tree/9-random_forest.py
+++ supervised_learning/decision_tree/9-random_forest.py
@@ -49,8 +49,8 @@ class Random_Forest():
     - Mean depth                     : {np.array(depths).mean()}
     - Mean number of nodes           : {np.array(nodes).mean()}
     - Mean number of leaves          : {np.array(leaves).mean()}
-    - Mean accuracy on training data : {np.array(accuracies).mean()}
-    - Accuracy of the forest on td   : {self.accuracy(self.explanatory, self.target)}""")
+    - Mean  on training data : {np.array(accuracies).mean()}
+    - Accuracy o on td   : {self.accuracy(self.explanatory, self.target)}""")
 
     def predict(self, explanatory):
         preds = np.array([pred(explanatory) for pred in self.numpy_preds])
