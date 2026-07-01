c50a87471a25ecd78a1e78c084bc044801321adc
lalalalily <ranaarasulzade@gmail.com>
2026-07-01 06:45:23 +0000

    hi

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Defines a neural network with one hidden layer performing binary classification
-with cost and propagation methods.
+and includes cost calculation.
 """
 import numpy as np
 
@@ -23,48 +23,49 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
+        # Private hidden layer weights, biases, and activation output
         self.__W1 = np.random.normal(size=(nodes, nx))
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
 
+        # Private output neuron weights, bias, and activation output
         self.__W2 = np.random.normal(size=(1, nodes))
         self.__b2 = 0
         self.__A2 = 0
 
     @property
     def W1(self):
-        """Getter W1"""
+        """Getter for W1"""
         return self.__W1
 
     @property
     def b1(self):
-        """Getter b1"""
+        """Getter for b1"""
         return self.__b1
 
     @property
     def A1(self):
-        """Getter A1"""
+        """Getter for A1"""
         return self.__A1
 
     @property
     def W2(self):
-        """Getter W2"""
+        """Getter for W2"""
         return self.__W2
 
     @property
     def b2(self):
-        """Getter b2"""
+        """Getter for b2"""
         return self.__b2
 
     @property
     def A2(self):
-        """Getter A2"""
+        """Getter for A2"""
         return self.__A2
 
     def forward_prop(self, X):
         """
         Calculates the forward propagation of the neural network
-        using the sigmoid activation function.
         """
         Z1 = np.matmul(self.__W1, X) + self.__b1
         self.__A1 = 1 / (1 + np.exp(-Z1))
@@ -76,10 +77,20 @@ class NeuralNetwork:
 
     def cost(self, Y, A):
         """
-        Calculates the cost of the model using logistic regression.
-        Uses 1.0000001 - A instead of 1 - A to avoid division by zero.
+        Calculates the cost of the model using logistic regression
+        
+        Parameters:
+        Y: numpy.ndarray with shape (1, m) containing the correct labels
+        A: numpy.ndarray with shape (1, m) containing the activated output
+        
+        Returns:
+        The cost value
         """
         m = Y.shape[1]
+        
+        # Logistic regression loss / Cross-entropy cost calculation
+        # Uses 1.0000001 - A to safeguard against log(0)
         loss = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
         cost = -1 / m * np.sum(loss)
+        
         return cost
