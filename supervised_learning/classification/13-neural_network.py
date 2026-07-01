8d68cbdc34970c48be5aa8e2a4fc3e68548f478e
lalalalily <ranaarasulzade@gmail.com>
2026-07-01 06:57:02 +0000

    hi

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -96,7 +96,7 @@ class NeuralNetwork:
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """
         Calculates one pass of gradient descent on the neural network.
-        
+
         Parameters:
         X: numpy.ndarray input data matrix
         Y: numpy.ndarray true target labels matrix
