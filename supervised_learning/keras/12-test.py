b796d83d26988d7e38038a2bc24ce8c4f0ecbbf0
lalalalily <ranaarasulzade@gmail.com>
2026-07-08 06:30:18 +0000

    hi

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module to evaluate a neural network's performance on test data
+"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    Tests a neural network model and evaluates its performance.
+
+    Parameters:
+    network: The network model to test.
+    data: The input data to test the model with.
+    labels: The correct one-hot labels of the data.
+    verbose: A boolean determining if output should be printed 
+             during the testing process.
+
+    Returns:
+    A list containing the loss and accuracy of the model, respectively.
+    """
+    return network.evaluate(data, labels, verbose=verbose)
