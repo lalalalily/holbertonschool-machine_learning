f2e5d12000660c17a495372511085b41e485e713
lalalalily <ranaarasulzade@gmail.com>
2026-07-08 06:23:58 +0000

    hi

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module to save and load model weights
+"""
+
+
+def save_weights(network, filename, save_format='keras'):
+    """
+    Saves a model's weights to a file.
+
+    Parameters:
+    network: The model whose weights should be saved.
+    filename: The path of the file that the weights should be saved to.
+    save_format: The format in which the weights should be saved.
+
+    Returns:
+    None
+    """
+    network.save_weights(filename, save_format=save_format)
+
+
+def load_weights(network, filename):
+    """
+    Loads a model's weights from a file.
+
+    Parameters:
+    network: The model to which the weights should be loaded.
+    filename: The path of the file that the weights should be loaded from.
+
+    Returns:
+    None
+    """
+    network.load_weights(filename)
