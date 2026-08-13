4a7e3e4d5aedb5c4a3a1ad072ba944a34734f541
lalalalily <ranaarasulzade@gmail.com>
2026-08-13 06:35:30 +0000

    la

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Defines the Yolo class for performing object detection using YOLOv3"""
+"""Yolo class - preprocess images"""
 import numpy as np
 import tensorflow.keras as K
 import glob
@@ -11,15 +11,7 @@ class Yolo:
     """Uses the Yolo v3 algorithm to perform object detection"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """
-        model_path: path to Darknet Keras model
-        classes_path: path to list of class names used for the Darknet
-            model
-        class_t: box score threshold for the initial filtering step
-        nms_t: IOU threshold for non-max suppression
-        anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2)
-            containing all of the anchor boxes
-        """
+        """Class constructor"""
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
             self.class_names = [line.strip() for line in f]
@@ -27,10 +19,30 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
-    def sigmoid(self, x):
-        """Applies the sigmoid function"""
+    @staticmethod
+    def sigmoid(x):
+        """Sigmoid activation function"""
         return 1 / (1 + np.exp(-x))
 
     def process_outputs(self, outputs, image_size):
-        """
-        outputs: list of numpy.ndarrays
+        """Process Darknet outputs"""
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+
+        image_height, image_width = image_size
+
+        for i, output in enumerate(outputs):
+            grid_h, grid_w, anchor_boxes, _ = output.shape
+
+            box = output[..., :4]
+            box_confidence = self.sigmoid(output[..., 4:5])
+            box_class_prob = self.sigmoid(output[..., 5:])
+
+            box_confidences.append(box_confidence)
+            box_class_probs.append(box_class_prob)
+
+            tx = box[..., 0]
+            ty = box[..., 1]
+            tw = box[..., 2]
+            th = box[..., 3]
