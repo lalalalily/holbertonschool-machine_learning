85d39c96cd2e5050016bdf87554b698a32cc71bb
lalalalily <ranaarasulzade@gmail.com>
2026-08-13 06:34:11 +0000

    la

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -1,14 +1,36 @@
-@staticmethod
-def load_images(folder_path):
-    """
-    folder_path: a string representing the path to the folder holding
-        all the images to load
+#!/usr/bin/env python3
+"""Defines the Yolo class for performing object detection using YOLOv3"""
+import numpy as np
+import tensorflow.keras as K
+import glob
+import cv2
+import os
 
-    Returns: (images, image_paths)
-        images: a list of images as numpy.ndarrays
-        image_paths: a list of paths to the individual images in images
-    """
-    image_paths = sorted(glob.glob(os.path.join(folder_path, '*')))
-    images = [cv2.imread(image_path) for image_path in image_paths]
 
-    return images, image_paths
+class Yolo:
+    """Uses the Yolo v3 algorithm to perform object detection"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        model_path: path to Darknet Keras model
+        classes_path: path to list of class names used for the Darknet
+            model
+        class_t: box score threshold for the initial filtering step
+        nms_t: IOU threshold for non-max suppression
+        anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2)
+            containing all of the anchor boxes
+        """
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid(self, x):
+        """Applies the sigmoid function"""
+        return 1 / (1 + np.exp(-x))
+
+    def process_outputs(self, outputs, image_size):
+        """
+        outputs: list of numpy.ndarrays
