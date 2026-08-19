73fafd97e8384bd8db237a58b3bf9114c8ce8596
lalalalily <ranaarasulzade@gmail.com>
2026-08-19 06:55:14 +0000

    hi

---
+++ unsupervised_learning/dimensionality_reduction/6-grads.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module that calculates the gradients of Y for t-SNE.
+"""
+import numpy as np
+Q_affinities = __import__('5-Q_affinities').Q_affinities
+
+
+def grads(Y, P):
+    """
+    Calculates the gradients of Y.
+
+    Y is a numpy.ndarray of shape (n, ndim) containing the low
+        dimensional transformation of X
+    P is a numpy.ndarray of shape (n, n) containing the P affinities
+        of X
+
+    Returns: (dY, Q)
+        dY is a numpy.ndarray of shape (n, ndim) containing the
+            gradients of Y
+        Q is a numpy.ndarray of shape (n, n) containing the Q
+            affinities of Y
+    """
+    n, ndim = Y.shape
+
+    Q, num = Q_affinities(Y)
+
+    PQ = P - Q
+
+    dY = np.zeros((n, ndim))
+    for i in range(n):
+        dY[i] = np.sum(
+            (PQ[:, i] * num[:, i])[:, np.newaxis] * (Y[i] - Y), axis=0
+        )
+
+    return dY, Q
