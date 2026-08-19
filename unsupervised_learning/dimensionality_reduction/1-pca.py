47e5c9cb4ddf27c6a10653ef951dbf108a5657c6
lalalalily <ranaarasulzade@gmail.com>
2026-08-19 06:47:10 +0000

    hi

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Performs PCA on a dataset.
+
+    X is a numpy.ndarray of shape (n, d) where:
+        n is the number of data points
+        d is the number of dimensions in each point
+    ndim is the new dimensionality of the transformed X
+
+    Returns: T, a numpy.ndarray of shape (n, ndim) containing the
+        transformed version of X
+    """
+    X_m = X - np.mean(X, axis=0)
+
+    u, s, vh = np.linalg.svd(X_m)
+
+    W = vh[:ndim].T
+
+    T = np.matmul(X_m, W)
+
+    return T
