e8e90534b53b9c7857b36208ea7f211344d3e728
lalalalily <ranaarasulzade@gmail.com>
2026-08-30 21:02:44 +0000

    new

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Sparse Autoencoder"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """Creates a sparse autoencoder
+    input_dims is an integer containing the dimensions of the model
+    input
+    hidden_layers is a list containing the number of nodes for each
+    hidden layer in the encoder, respectively
+    the hidden layers should be reversed for the decoder
+    latent_dims is an integer containing the dimensions of the latent
+    space representation
+    lambtha is the regularization parameter used for L1
+    regularization on the encoded output
+    Returns: encoder, decoder, auto
+    encoder is the encoder model
+    decoder is the decoder model
+    auto is the sparse autoencoder model
+    """
+    reg = keras.regularizers.l1(lambtha)
+
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    x = encoder_inputs
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    latent = keras.layers.Dense(latent_dims, activation='relu',
+                                 activity_regularizer=reg)(x)
+    encoder = keras.Model(encoder_inputs, latent)
+
+    decoder_inputs = keras.Input(shape=(latent_dims,))
+    x = decoder_inputs
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    decoder_outputs = keras.layers.Dense(input_dims,
+                                          activation='sigmoid')(x)
+    decoder = keras.Model(decoder_inputs, decoder_outputs)
+
+    auto_outputs = decoder(encoder(encoder_inputs))
+    auto = keras.Model(encoder_inputs, auto_outputs)
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
