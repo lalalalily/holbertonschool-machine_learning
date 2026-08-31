276dd622bdd78f3d7b6a9c6ac3ddbd6925a325de
lalalalily <ranaarasulzade@gmail.com>
2026-08-31 06:33:50 +0000

    hi

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,43 +1,52 @@
 #!/usr/bin/env python3
-"""Monte Carlo algorithm"""
+"""
+Monte Carlo evaluation algorithm
+"""
 import numpy as np
 
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
     """
-    Performs the Monte Carlo algorithm
+    Performs the Monte Carlo algorithm to estimate value functions.
 
-    env: environment instance
-    V: numpy.ndarray of shape (s,) containing the value estimate
-    policy: function that takes in a state and returns the next action
-    episodes: total number of episodes to train over
-    max_steps: maximum number of steps per episode
-    alpha: learning rate
-    gamma: discount rate
+    Parameters:
+        env: environment instance
+        V: numpy.ndarray of shape (s,) containing the value estimate
+        policy: function that takes in a state and returns next action
+        episodes: total number of episodes to train over
+        max_steps: maximum number of steps per episode
+        alpha: learning rate
+        gamma: discount rate
 
-    Returns: V, the updated value estimate
+    Returns:
+        V: updated value estimate
     """
-    for ep in range(episodes):
+    for episode in range(episodes):
         state, _ = env.reset()
-        episode = []
+        episode_data = []
 
+        # Generate an episode
         for step in range(max_steps):
             action = policy(state)
             next_state, reward, terminated, truncated, _ = env.step(action)
-            episode.append((state, reward))
-            state = next_state
+            episode_data.append((state, reward))
+            
             if terminated or truncated:
                 break
+            state = next_state
 
-        episode = np.array(episode, dtype=int)
-        states = episode[:, 0]
+        # Backpropagate updates using First-Visit Monte Carlo
         G = 0
-        for t in range(len(episode) - 1, -1, -1):
-            state, reward = episode[t]
+        visited_states = set()
+        
+        for state, reward in reversed(episode_data):
             G = reward + gamma * G
-            # first-visit: only update if this is the first occurrence
-            # of `state` in the episode up to and including index t
-            if state not in states[:t]:
-                V[state] = V[state] + alpha * (G - V[state])
+            if state not in visited_states:
+                visited_states.add(state)
+                if alpha is not None:
+                    V[state] = V[state] + alpha * (G - V[state])
+                else:
+                    # Fallback standard sample average if alpha is set to None
+                    V[state] = V[state] + (1 / episodes) * (G - V[state])
 
     return V
