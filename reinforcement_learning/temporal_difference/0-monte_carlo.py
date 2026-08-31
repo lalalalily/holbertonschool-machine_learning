c3f8e112800a6e91eb2b40f0d754790afa38ff3f
lalalalily <ranaarasulzade@gmail.com>
2026-08-31 06:38:25 +0000

    hi

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,52 +1,27 @@
 #!/usr/bin/env python3
-"""
-Monte Carlo evaluation algorithm
-"""
+"""Monte Carlo algorithm"""
 import numpy as np
 
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
-    """
-    Performs the Monte Carlo algorithm to estimate value functions.
-
-    Parameters:
-        env: environment instance
-        V: numpy.ndarray of shape (s,) containing the value estimate
-        policy: function that takes in a state and returns next action
-        episodes: total number of episodes to train over
-        max_steps: maximum number of steps per episode
-        alpha: learning rate
-        gamma: discount rate
-
-    Returns:
-        V: updated value estimate
-    """
-    for episode in range(episodes):
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
+                 alpha=0.1, gamma=0.99):
+    """Performs the Monte Carlo algorithm"""
+    for ep in range(episodes):
         state, _ = env.reset()
-        episode_data = []
-
-        # Generate an episode
+        episode = []
         for step in range(max_steps):
             action = policy(state)
             next_state, reward, terminated, truncated, _ = env.step(action)
-            episode_data.append((state, reward))
-            
+            episode.append((state, reward))
+            state = next_state
             if terminated or truncated:
                 break
-            state = next_state
-
-        # Backpropagate updates using First-Visit Monte Carlo
+        episode = np.array(episode, dtype=int)
         G = 0
-        visited_states = set()
-        
-        for state, reward in reversed(episode_data):
+        states = episode[:, 0]
+        for i, (state, reward) in enumerate(episode[::-1]):
             G = reward + gamma * G
-            if state not in visited_states:
-                visited_states.add(state)
-                if alpha is not None:
-                    V[state] = V[state] + alpha * (G - V[state])
-                else:
-                    # Fallback standard sample average if alpha is set to None
-                    V[state] = V[state] + (1 / episodes) * (G - V[state])
-
+            idx = len(episode) - 1 - i
+            if state not in states[:idx]:
+                V[state] = V[state] + alpha * (G - V[state])
     return V
