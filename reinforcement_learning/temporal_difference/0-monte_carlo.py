8eab176116565eee88438d052b66e2d936e2bc39
lalalalily <ranaarasulzade@gmail.com>
2026-08-31 07:16:21 +0000

    hi

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
-"""Monte Carlo algorithm"""
+"""Monte Carlo algorithm """
 import numpy as np
 
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
-    """
-    Performs the Monte Carlo algorithm
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
+                gamma=0.99):
+    """Perform the Monte Carlo algorithm.
 
-    env: environment instance
-    V: numpy.ndarray of shape (s,) containing the value estimate
-    policy: function that takes in a state and returns the next action
-    episodes: total number of episodes to train over
-    max_steps: maximum number of steps per episode
-    alpha: learning rate
-    gamma: discount rate
-
+    env is environment instance
+    V is a numpy.ndarray of shape (s,) containing the value estimate
+    policy is a function that takes in a state and returns the next
+        action to take
+    episodes is the total number of episodes to train over
+    max_steps is the maximum number of steps per episode
+    alpha is the learning rate
+    gamma is the discount rate
     Returns: V, the updated value estimate
     """
     for ep in range(episodes):
-        state, _ = env.reset()
+        state = env.reset()[0]
         episode = []
 
         for step in range(max_steps):
             action = policy(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            episode.append((state, reward))
-            state = next_state
-            if terminated or truncated:
+            next_state, reward, done, truncated, _ = env.step(action)
+            episode.append([state, action, reward, next_state])
+            if done:
                 break
+            state = next_state
 
         episode = np.array(episode, dtype=int)
-        states = episode[:, 0]
+
         G = 0
-        for t in range(len(episode) - 1, -1, -1):
-            state, reward = episode[t]
-            G = reward + gamma * G
-            if state not in states[:t]:
+        for step in reversed(range(len(episode))):
+            state, action, reward, next_state = episode[step]
+            G = gamma * G + reward
+            if state not in episode[:ep, 0]:
                 V[state] = V[state] + alpha * (G - V[state])
 
     return V
